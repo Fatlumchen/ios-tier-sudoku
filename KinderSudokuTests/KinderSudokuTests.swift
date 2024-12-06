52d21b21210403dde6db606590d12d233288811b
Fatljum Neziri <143098133+Fatlumchen@users.noreply.github.com>
2024-12-06 16:46:55 +0100

    Initial Commit

---
+++ KinderSudokuTests/KinderSudokuTests.swift
@@ -0,0 +1,17 @@
+//
+//  KinderSudokuTests.swift
+//  KinderSudokuTests
+//
+//  Created by Fatljum Neziri on 06.12.24.
+//
+
+import Testing
+@testable import KinderSudoku
+
+struct KinderSudokuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
